5618938ffc3ae087378ca63d19378bd4323b3ed8
Ayush <ayush.gupta@almondz.com>
2024-10-09 12:39:54 +0530

    add module

---
+++ my_product_discount_ecommerce/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Product Discount for eCommerce',
+    'version': '14.0.1',
+    'summary': 'Add Discount Percentage to Products and Display on eCommerce',
+    'category': 'Sales',
+    'author': 'Ayush Gupta',
+    'depends': ['product', 'website_sale'],
+    'data': [
+        'views/website_sale_template.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
